0165cd9f084a5041aa846d8ba6f792069ae8776a
Andrew Petersen <kirbysayshi@gmail.com>
2013-10-22 00:15:58 -0400

    BoidData as separate module

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 
-var work = require('webworkify')
-  , SSI = require('./lib/ssi')
-
-var statshelper = require('./lib/statshelper');
+var work = require('webworkify');
 
 var cvs = document.querySelector('#stage')
   , ctx = cvs.getContext('2d');
+  , resizemon = require('./lib/resizemon')(cvs);
 
-var resizemon = require('./lib/resizemon')(cvs);
+var statshelper = require('./lib/statshelper')
+  , renderStats = statshelper()
+  , physStats = statshelper();
 
-var renderStats = statshelper();
-var physStats = statshelper();
-
+var BoidData = require('./lib/boiddata')
+  , SSI = require('./lib/ssi');
 
 var worker = work(require('./lib/worker'));
-
-var knownBoids = {};
 
 worker.addEventListener('message', function(ev) {
 
   if (ev.data.type === 'new boid') {
-    initializeBoidStruct(ev.data.id, ev.data.update);
-
-    // This is for initial debugging purposes.
-    drawBoid(knownBoids[ev.data.id], 1);
+    new BoidData(ev.data.update);
     return;
   }
 
   if (ev.data.type === 'boid updates') {
     for (var i = 0; i < ev.data.updates.length; i++) {
-      updateBoidStruct(ev.data.updates[i]);
+      BoidData.update(ev.data.updates[i])
     }
     physStats.end();
     return;
@@ -37,60 +31,10 @@ worker.addEventListener('message', function(ev) {
 
 });
 
-function initializeBoidStruct(id, data) {
-  // Mark this boid as "known"
-  knownBoids[id] = boidStruct(id);
-  // Do the same as if we're just updating the boid on a tick.
-  var boidData = updateBoidStruct(data);
-  // Manually set the previous position to current to avoid interpolating
-  // between 0 and current.
-  boidData.px = boidData.cx;
-  boidData.py = boidData.cy;
-}
-
-function boidStruct(id) {
-  return {
-    id: id,
-    px: 0,
-    py: 0,
-    cx: 0,
-    cy: 0,
-    radius: 0
-  }
-}
-
-function updateBoidStruct(data) {
-  var boidData = knownBoids[data.id];
-  boidData.px = boidData.cx;
-  boidData.py = boidData.cy;
-  boidData.cx = data.x;
-  boidData.cy = data.y;
-  boidData.radius = data.radius;
-  return boidData;
-}
-
-function drawBoid(boid, ratio) {
-  var oneMinusRatio = 1 - ratio;
-  var x = (boid.cx * ratio) + (boid.px * oneMinusRatio);
-  var y = (boid.cy * ratio) + (boid.py * oneMinusRatio);
-  ctx.fillStyle = 'rgba(0,0,255, 0.3)';
-  ctx.beginPath();
-  ctx.arc(x, y, boid.radius, 0, Math.PI*2, false);
-  ctx.fill();
-}
-
 function graphics(dt, ratio) {
   renderStats.begin();
-  var boidIds = Object.keys(knownBoids)
-    , boid
-    , i;
-
   ctx.clearRect(0, 0, cvs.width, cvs.height);
-
-  for (i = 0; i < boidIds.length; i++) {
-    boid = knownBoids[boidIds[i]];
-    drawBoid(boid, ratio);
-  }
+  BoidData.drawAll(ctx, ratio);
   renderStats.end();
 }
 
@@ -98,7 +42,6 @@ function logics(dt) {
   physStats.begin();
   worker.postMessage({ type: 'logics', dt: dt });
 }
-
 
 var ssi = new SSI(1000 / 30, logics, graphics);
 
